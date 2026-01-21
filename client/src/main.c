29acb4f6a4d77d8dbd993b6db256f18bed2dd840
amadene-git <adioumd@hotmail.fr>
2026-01-21 14:04:20 +0100

    Add resolve DNS

--- client/src/main.c
+++ client/src/main.c
@@ -9,15 +9,15 @@ int main(int ac, char** av) {
     return 1;
   }
 
-  t_rawSocket* rawSocket = initializeRawSocket(av[1]);
+  t_rawSocket* server = NULL;
+  t_rawSocket* rawSocket = initializeRawSocket(av[1], server);
   printf("ft_pîng: socket initialized\n");
 
-  const char* buffer = "HELLO";
+  const char* buffer = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   int ret =
-      sendto(rawSocket->_sockfd, buffer, 5, MSG_CONFIRM,
+      sendto(rawSocket->_sockfd, buffer, 26, MSG_CONFIRM,
              (struct sockaddr*)(&rawSocket->_sockAddr), rawSocket->_socklen);
 
   printf("ft_pîng: send %d bytes\n", ret);
-
 	
 }
--- utils/src/rawSocket.c
+++ utils/src/rawSocket.c
@@ -3,9 +3,20 @@
 
 #include <arpa/inet.h>
 #include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+/*
+int getaddrinfo(const char *node, const char *service,
+                const struct addrinfo *hints,
+                struct addrinfo **res);
 
-t_rawSocket* initializeRawSocket(const char* bindingAddress) {
-  t_rawSocket* server = NULL;
+void freeaddrinfo(struct addrinfo *res);
+
+const char *gai_strerror(int errcode);
+*/
+
+t_rawSocket* initializeRawSocket(const char *host, t_rawSocket* server) {
   if ((server = malloc(sizeof(t_rawSocket))) == NULL) {
     exitError("malloc() failed");
   }
@@ -13,25 +24,40 @@ t_rawSocket* initializeRawSocket(const char* bindingAddress) {
   if ((server->_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) {
     exitError("socket() failed");
   }
+
+  resolveDNS(host, &server->_sockAddr);
   server->_sockAddr.sin_family = AF_INET;
   server->_socklen = sizeof(struct sockaddr_in);
-
-  int pton_ret = 0;
-  if ((pton_ret = inet_pton(AF_INET, bindingAddress,
-                            (void*)(&server->_sockAddr.sin_addr))) <= 0) {
-    if (pton_ret == 0) {
-      exitError("Error: Invalid Ip Address");
-    } else {
-
-      exitError("inet_pton() failed");
-    }
-  }
-  bzero(server->_ipAddress, 16);
-  strcpy(server->_ipAddress, bindingAddress);
 
   if (bind(server->_sockfd, (struct sockaddr*)(&server->_sockAddr),
            server->_socklen) == -1) {
     exitError("bind() failed");
   }
   return server;
+}
+
+int resolveDNS(const char *host, struct sockaddr_in *addrin) {
+	struct addrinfo hints;
+    struct addrinfo *result= NULL;
+	memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_RAW; /* Datagram socket */
+    hints.ai_protocol = IPPROTO_ICMP;          /* Any protocol */
+   
+	printf("try resolve DNS: %s\n", host);
+	
+	int ret = getaddrinfo(host, NULL, &hints, &result);
+
+	if (ret != 0 || result == NULL){
+		fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(ret));
+		return (-1);
+	}
+
+	*addrin = *(struct sockaddr_in *)result->ai_addr;
+	char str_ip[100] = {0};
+	inet_ntop(AF_INET, &addrin->sin_addr.s_addr,
+                      &str_ip[0], 100);
+	printf("resolve dns make: %s -> %s\n", host, str_ip);
+
+	return 0;
 }
