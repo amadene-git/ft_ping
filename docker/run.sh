701703434a10f39331486401d97483fd293bc806
Adrien Madene <adrien.madene@novasparks.com>
2026-04-28 02:11:47 +0000

    Add review + Fix sudo ./ft_ping

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,8 @@
-docker run -it --rm --name ${USER}_ft_ping --user $USER --hostname ${HOSTNAME}_ft_ping -w $PWD -v $HOME:$HOME  ft_ping_image
+docker run -it --rm \
+  --name ${USER}_ft_ping \
+  --user $USER \
+  --cap-add=NET_RAW \
+  --hostname ${HOSTNAME}_ft_ping \
+  -w $PWD \
+  -v $HOME:$HOME \
+  ft_ping_image
